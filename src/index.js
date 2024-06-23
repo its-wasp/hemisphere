6d6beb352a916ed3d23591be3ec55b2e8a2066e3
its-wasp <its_wasp@outlook.com>
2024-06-23 23:39:16 +0530

    added state

--- src/index.js
+++ src/index.js
@@ -2,14 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component{
-    render(){
+    constructor(props){
+        super(props)
+
+        this.state = { latitude:null}
+
         window.navigator.geolocation.getCurrentPosition(
-            (position) => console.log(position),
+            (position) => {
+                this.setState({ latitude:position.coords.latitude})
+            },
             (error) => console.log(error)
         );
+    }
+    render(){
         return (
             <div>
-                You are in northern hemisphere !
+                { this.state.latitude }
             </div>
         )
     }
